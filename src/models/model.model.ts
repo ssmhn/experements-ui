3565c2cfde831d29c8f9e530b1ff0b18be06a632
matvey.samokhin <matvey.samokhin@engee.cloud>
2024-03-18 21:56:41 +0300

    add model types

--- src/models/model.model.ts
+++ src/models/model.model.ts
@@ -2,8 +2,27 @@ import {ModelInterface} from "../types/model.type";
 import {ModelDTO} from "../types/dto/model.dto";
 
 export class ModelClass implements ModelInterface {
+    uuid: string
+    name: string
+    status: string
+    predictLabel: string
+    distributions: string
+    algorithm: string
+    errorMessage: string
+    hyperparams: string
+    encoders: string
+    scalers: string
 
     constructor(dto: ModelDTO, id?: string) {
-
+        this.uuid = dto.uuid || id
+        this.name = dto.name || ''
+        this.status = dto.status || ''
+        this.predictLabel = dto.predictLabel || ''
+        this.distributions = dto.distributions || ''
+        this.algorithm = dto.algorithm || ''
+        this.errorMessage = dto.errorMessage || ''
+        this.hyperparams = dto.hyperparams || ''
+        this.encoders = dto.encoders || ''
+        this.scalers = dto.scalers || ''
     }
 }
--- src/types/model.type.ts
+++ src/types/model.type.ts
@@ -1,13 +1,40 @@
 import * as t from 'io-ts';
 
 export const ModelType = t.type({
-
+    uuid: t.string,
+    name: t.string,
+    status: t.string,
+    predictLabel: t.string,
+    distributions: t.string,
+    algorithm: t.string,
+    errorMessage: t.string,
+    hyperparams: t.string,
+    encoders: t.string,
+    scalers: t.string
 });
 
 export type Model = {
-
+    uuid: string,
+    name: string,
+    status: string,
+    predictLabel: string,
+    distributions: string,
+    algorithm: string,
+    errorMessage: string,
+    hyperparams: string,
+    encoders: string,
+    scalers: string
 }
 
 export interface ModelInterface {
-
+    uuid: string,
+    name: string,
+    status: string,
+    predictLabel: string,
+    distributions: string,
+    algorithm: string,
+    errorMessage: string,
+    hyperparams: string,
+    encoders: string,
+    scalers: string
 }
